e6c32d3a14b63675ac850346732dca7c6c23678d
Anxo Rodriguez <anxolin@gmail.com>
2019-12-27 14:39:41 +0100

    Print networks

---
+++ src/scripts/printAddresses.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const networksJson = require('@gnosis.pm/dex-contracts/networks.json')
+const { CONTRACTS } = require('../const')
+CONTRACTS.forEach(contractName => {
+  const networks = networksJson[contractName]
+
+  console.log(contractName + ':')
+  Object.entries(networks).forEach(([ networkId,  network]) => {
+    console.log(`    ${networkId}: ${networks[networkId].address}`)
+  })
+})
+console.log()
