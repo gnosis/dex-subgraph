234156a5099dfab0bb72e923ee7a9258adcebd88
Anxo Rodriguez <anxolin@gmail.com>
2019-12-27 13:58:31 +0100

    Delete migration and contracts

--- contracts/DevDependencies.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.5.2;
-
-// import "@gnosis.pm/dex-contracts/contracts/BatchExchange.sol";
-
-
-contract DxDevDependencies {}
--- migrations/2_deploy_contract.js
+++
@@ -1,9 +0,0 @@
-/* global artifacts, error, web3 */
-
-const dependencies = require('@gnosis.pm/dex-contracts/src/migration/dependencies')
-const migrateBatchExchange = require('@gnosis.pm/dex-contracts/src/migration/PoC_dfusion')
-
-module.exports = async function(deployer, network, accounts) {
-  await dependencies({ artifacts, deployer, network })
-  await migrateBatchExchange({ artifacts, deployer, network, accounts, web3 })
-}
