a72e80bdf62022c526fef065928b2307e701a87b
Anxo Rodriguez <anxolin@gmail.com>
2019-12-31 12:50:30 +0100

    Refactor token creator to be able to reuse the logic

--- src/mappings/tokens.ts
+++ src/mappings/tokens.ts
@@ -1,23 +1,28 @@
-import { log } from '@graphprotocol/graph-ts'
+import { log, Address, BigInt, Bytes } from '@graphprotocol/graph-ts'
 import { Token } from '../../generated/schema'
 import { AddTokenCall } from '../../generated/BatchExchange/BatchExchange'
 import { epochToBatchId } from '../utils'
 
 export function onAddToken(call: AddTokenCall): void {
-  // Create order
   let address = call.inputs.token
+  let timestamp = call.block.timestamp
+  let txHash = call.transaction.hash
+  createToken(address, timestamp, txHash)
+}
+
+
+export function createToken(address: Address, timestamp: BigInt, txHash: Bytes): void {
+  // Create token
   log.info('[onAddToken] Create Token: {}', [address.toHex()])
   let token = new Token(address.toHex())
   token.address = address
-  token.fromBatchId = epochToBatchId(call.block.timestamp)
+  token.fromBatchId = epochToBatchId(timestamp)
 
   // Audit dates
-  token.createEpoch = call.block.timestamp
-  // cancelEpoch: BigInt!
-  // deleteEpoch: BigInt!
+  token.createEpoch = timestamp
 
   // Transaction
-  token.txHash = call.transaction.hash
-  
+  token.txHash = txHash
+
   token.save()
 }
