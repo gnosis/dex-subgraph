629d957f36e4d14a77b530fec959c5042ad51bf6
Anxo Rodriguez <anxolin@gmail.com>
2019-12-31 12:57:58 +0100

    Refactor params

---
+++ src/mappings/trades.ts
@@ -0,0 +1,47 @@
+import { log } from '@graphprotocol/graph-ts'
+import { Trade as TradeEvent } from '../../generated/BatchExchange/BatchExchange'
+import { Trade, Order } from '../../generated/schema'
+import { toOrderId, epochToBatchId, toTradeId, batchIdToEpoch } from '../utils'
+
+export function onTrade(event: TradeEvent): void { 
+  let params = event.params;
+
+  // Calculate batchId
+  let createEpoch = event.block.timestamp
+  let batchId = epochToBatchId(createEpoch)
+
+  // Create order
+  let tradeId = toTradeId(params.owner, params.orderId, batchId)
+  log.info('[onOrderPlacement] Create Trade: {}', [tradeId])
+  let trade = new Trade(tradeId)  
+
+  // Set relationship with order
+  let orderId = toOrderId(params.owner, params.orderId)  
+  trade.buyVolume = params.executedBuyAmount
+  trade.order = orderId
+  let orderOpt = Order.load(orderId)
+  if (!orderOpt) {
+    throw new Error("Order doesn't exist: " + orderId)
+  }
+  let order = orderOpt!
+  let sellVolume = params.executedSellAmount
+  let buyVolume = params.executedBuyAmount
+  order.soldAmount = order.soldAmount.plus(sellVolume)
+  order.trades.push(tradeId)
+  
+  // Trade details
+  trade.sellVolume = sellVolume
+  trade.buyVolume = buyVolume
+  trade.tradeBatchId = batchId
+  trade.tradeEpoch = batchIdToEpoch(batchId)
+
+  // Audit dates
+  trade.createEpoch = createEpoch
+  trade.revertEpoch = null
+
+  // Transaction
+  trade.txHash = event.transaction.hash
+  trade.txLogIndex = event.transactionLogIndex 
+  
+  trade.save()
+}
