f8332643f053a978a89d15a89cec46467710580d
Anxo Rodriguez <anxolin@gmail.com>
2020-01-03 19:33:31 +0100

    Implement user mapping

---
+++ src/mappings/user.ts
@@ -0,0 +1,35 @@
+import { log, Address, BigInt, Bytes, EthereumEvent } from '@graphprotocol/graph-ts'
+import { User } from '../../generated/schema'
+import { epochToBatchId } from '../utils'
+
+export function createUserIfNotCreated(address: Address, event: EthereumEvent): User {
+  const id = address.toHex()
+  let user = User.load(id)
+  log.info('[createUserIfNotCreated] Get User: {}', [id])
+  if (user == null) {
+    let timestamp = event.block.timestamp
+    let txHash = event.transaction.hash
+    
+    user = _createUser(id, timestamp, txHash)
+  }
+
+  return user!
+}
+
+
+function _createUser(id: string, timestamp: BigInt, txHash: Bytes): User {
+  log.info('[createUser] Create User {}', [id])
+
+  // Create token  
+  let user = new User(id)
+  user.fromBatchId = epochToBatchId(timestamp)
+
+  // Audit dates
+  user.createEpoch = timestamp
+
+  // Transaction
+  user.txHash = txHash
+
+  user.save()
+  return user
+}
