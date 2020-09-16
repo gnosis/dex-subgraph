5d8e79a5e19b7eab4f0e69a5538999de187a3699
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-09-16 10:54:16 +0200

    setup the scripts to use TS

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,15 +1,4 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'prettier/@typescript-eslint',
-  ],
   rules: {
     // NOTE: AssemblyScript doesn't have the same non-null type guard logic.
     '@typescript-eslint/no-non-null-assertion': 'off',
