02bd1ec44671351bced480f4c2aba425d2c6cbdf
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-09-20 20:51:27 +0200

    improve deposits test coverage

--- tests/wasm/entities.ts
+++ tests/wasm/entities.ts
@@ -1,99 +1,105 @@
 import { fromHex, toHex } from './runtime/convert'
 import { Entity, Entry, Value, ValueKind } from './runtime/store'
 
-export interface User {
-  id: string
-  fromBatchId: bigint
-  createEpoch: bigint
-  txHash: string
+const Kinds = {
+  User: {
+    id: ValueKind.String,
+    fromBatchId: ValueKind.BigInt,
+    createEpoch: ValueKind.BigInt,
+    txHash: ValueKind.Bytes,
+  },
+  Deposit: {
+    id: ValueKind.String,
+    user: ValueKind.String,
+    tokenAddress: ValueKind.Bytes,
+    amount: ValueKind.BigInt,
+    batchId: ValueKind.BigInt,
+    createEpoch: ValueKind.BigInt,
+    txHash: ValueKind.Bytes,
+  },
+} as const
+
+type ValueOf<T> = T extends ValueKind.Array
+  ? never
+  : T extends ValueKind.Bytes
+  ? string
+  : T extends readonly [infer S]
+  ? ValueOf<S>[]
+  : Extract<Value, { kind: T }>['data']
+
+type EntityData<T> = {
+  [K in keyof T]: ValueOf<T[K]>
 }
 
-/* eslint-disable prettier/prettier */
-interface Entities {
-  'User': User
-}
-/* eslint-enable prettier/prettier */
+export type Names = keyof typeof Kinds
+export type Data<K extends Names> = EntityData<typeof Kinds[K]>
 
-export type Names = keyof Entities
-export type Data<T extends Names> = Entities[T]
-
-export function toData<T extends Names>(name: T, entity: Entity): Data<T> {
-  switch (name) {
-    case 'User':
-      return {
-        id: getValueOf(entity, 'id', ValueKind.String),
-        fromBatchId: getValueOf(entity, 'fromBatchId', ValueKind.BigInt),
-        createEpoch: getValueOf(entity, 'createEpoch', ValueKind.BigInt),
-        txHash: getValueOf(entity, 'txHash', ValueKind.Bytes),
-      }
-    default:
-      throw new Error(`unknown entity ${name}`)
-  }
-}
-
-export function fromData<T extends Names>(name: T, data: Data<T>): Entity {
-  switch (name) {
-    case 'User':
-      return {
-        entries: [
-          getEntry(<User>data, 'id', ValueKind.String),
-          getEntry(<User>data, 'fromBatchId', ValueKind.BigInt),
-          getEntry(<User>data, 'createEpoch', ValueKind.BigInt),
-          getEntry(<User>data, 'txHash', ValueKind.Bytes),
-        ],
-      }
-    default:
-      throw new Error(`unknown entity ${name}`)
-  }
-}
-
-type ValueOf<T extends ValueKind> = T extends ValueKind.Bytes ? string : Extract<Value, { kind: T }>['data']
-
-function toValueOf<T extends ValueKind>(value: Value, kind: T): ValueOf<T> {
-  if (value.kind === kind) {
-    // NOTE: Casting is necessary since as the type checker doesn't realize
-    // that `ValueOf<kind> === ValueOf<value.kind>`.
-    switch (value.kind) {
-      case ValueKind.Bytes:
-        return toHex(value.data) as ValueOf<T>
-      default:
-        return value.data as ValueOf<T>
+export function toData<K extends Names>(name: K, entity: Entity): Data<K> {
+  const data: Record<string, unknown> = {}
+  for (const [key, kind] of Object.entries(Kinds[name])) {
+    const entry = entity.entries.find(({ name }) => name === key)
+    if (entry === undefined) {
+      throw new Error(`entity missing '${key}' entry`)
     }
-  } else {
+
+    data[key] = coerceFromValue(entry.value, kind)
+  }
+
+  return data as Data<K>
+}
+
+export function fromData<K extends Names>(name: K, data: Data<K>): Entity {
+  const entries: Entry[] = []
+  const dataProperties = data as Record<string, unknown>
+  for (const [key, kind] of Object.entries(Kinds[name])) {
+    entries.push({ name: key, value: coerceToValue(dataProperties[key], kind) })
+  }
+
+  return { entries }
+}
+
+type RecursiveValueKind = Exclude<ValueKind, ValueKind.Array> | readonly [RecursiveValueKind]
+
+function isRecursive(kind: RecursiveValueKind): kind is readonly [RecursiveValueKind] {
+  return Array.isArray(kind)
+}
+
+function coerceFromValue(value: Value, kind: RecursiveValueKind): unknown {
+  const unexpectedKindError = (expectedKind: ValueKind) => {
     const n = (kind: ValueKind) => ValueKind[kind] || 'unknown'
-    throw new Error(`expected ${n(kind)} store value but got ${n(value.kind)}`)
+    return new Error(`expected ${n(expectedKind)} store value but got ${n(value.kind)}`)
+  }
+
+  if (isRecursive(kind)) {
+    if (value.kind !== ValueKind.Array) {
+      throw unexpectedKindError(ValueKind.Array)
+    }
+    return value.data.map((value) => coerceFromValue(value, kind[0]))
+  }
+
+  if (value.kind !== kind) {
+    throw unexpectedKindError(kind)
+  }
+  switch (value.kind) {
+    case ValueKind.Bytes:
+      return toHex(value.data)
+    default:
+      return value.data
   }
 }
 
-function getValueOf<T extends ValueKind>(entity: Entity, key: string, kind: T): ValueOf<T> {
-  const entry = entity.entries.find(({ name }) => name === key)
-  if (entry === undefined) {
-    throw new Error(`entity missing '${key}' entry`)
+function coerceToValue(data: unknown, kind: RecursiveValueKind): Value {
+  if (isRecursive(kind)) {
+    return {
+      kind: ValueKind.Array,
+      data: (data as unknown[]).map((data) => coerceToValue(data, kind[0])),
+    }
   }
-  return toValueOf(entry.value, kind)
-}
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function getArrayOf<T extends ValueKind>(entity: Entity, key: string, kind: T): ValueOf<T>[] {
-  // NOTE: We have to cast the closure to `any` to appease the type checker.
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return getValueOf(entity, key, ValueKind.Array).map(((v: Value) => toValueOf(v, kind)) as any)
-}
-
-function fromValueOf<T extends ValueKind>(data: ValueOf<T>, kind: T): Value {
   switch (kind) {
     case ValueKind.Bytes:
-      return { kind, data: fromHex(data as string) } as Value
+      return { kind, data: fromHex(data as string) }
     default:
       return { kind, data } as Value
-  }
-}
-
-type KindOf<T> = T extends string ? ValueKind.String | ValueKind.Bytes : Extract<Value, { data: T }>['kind']
-
-function getEntry<T, K extends keyof T>(data: T, key: K, kind: KindOf<T[K]>): Entry {
-  return {
-    name: key.toString(),
-    value: fromValueOf(data[key] as ValueOf<typeof kind>, kind),
   }
 }
