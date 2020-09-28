dc72b4b825aa80e743feea8b21a083cb20f31d7f
Nicholas Rodrigues Lordello <nicholas.lordello@gnosis.pm>
2020-09-28 13:18:08 +0200

    use token listing event

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -1,4 +1,4 @@
-export { onAddToken } from './tokens'
+export { onTokenListing } from './tokens'
 export { onOrderPlacement, onOrderCancellation, onOrderDeletion } from './orders'
 export { onTrade, onTradeReversion } from './trades'
 export { onDeposit } from './deposits'
