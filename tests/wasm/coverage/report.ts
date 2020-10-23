2a83bf090350e3d5e8b0c5b80d97b107883f6d3f
Felix Leupold <felixleupold90@gmail.com>
2020-10-23 16:54:34 +0200

    using travis job id

--- tests/wasm/coverage/report.ts
+++ tests/wasm/coverage/report.ts
@@ -23,7 +23,7 @@ export class Report {
     if (!data) {
       data = {
         repo_token: process.env.COVERALLS_REPO_TOKEN,
-        service_job_id: process.env.TRAVIS_BUILD_ID,
+        service_job_id: process.env.TRAVIS_JOB_ID,
         service_name: 'travis-pro',
         source_files: [],
       }
