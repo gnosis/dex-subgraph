b6c2cf4e12b61c036c007a0927e9a2ebbbc5563b
Felix Leupold <felixleupold90@gmail.com>
2020-10-26 19:04:09 +0100

    [Coverage 1] Coveralls Report format (#121)
    This PR is the first one on a series to support code coverage for our assembly script graph handling code (see result here: https://coveralls.io/jobs/69608663)
    
    First, we create a helper class that can create, write and manage a coverage report in the format that coveralls accepts it. I used their [documentation](https://docs.coveralls.io/api-introduction) to get the required format.
    
    Coverage is achieved by rewriting the ASC code with [`trace` statements](https://www.assemblyscript.org/debugging.html#manual-tracing) before each meaningful expression (e.g. line of code) and implementing the `trace` call in our runtime to translate every invocation in line coverage.
    
    The rewrite logic will add each ASC file to the empty report struct via `addFile`. The runtime will implement `trace` by calling into `recordCoverage`.
    
    ### Test Plan
    
    At the end of the PR stack we can run `yarn coverage` to generate a report.

---
+++ tests/wasm/coverage/report.ts
@@ -0,0 +1,60 @@
+import fs from 'fs'
+import md5File from 'md5-file'
+import { InstrumentedLines } from './'
+
+const COVERAGE_FOLDER = './coverage/'
+const COVERAGE_FILE = COVERAGE_FOLDER + 'report.json'
+
+interface ReportData {
+  repo_token: string | undefined
+  service_job_id: string | undefined
+  service_name: string
+  source_files: {
+    name: string
+    source_digest: string
+    coverage: (number | null)[]
+  }[]
+}
+
+export class Report {
+  data: ReportData
+
+  constructor(data?: ReportData) {
+    if (!data) {
+      data = {
+        repo_token: process.env.COVERALLS_REPO_TOKEN,
+        service_job_id: process.env.TRAVIS_BUILD_ID,
+        service_name: 'travis-ci',
+        source_files: [],
+      }
+    }
+    this.data = data
+  }
+
+  addFile(file: string, instrumentedLines: InstrumentedLines): void {
+    const source_digest = md5File.sync(file)
+    this.data.source_files.push({
+      name: file,
+      source_digest,
+      coverage: instrumentedLines.map((isInstrumented: boolean) => (isInstrumented ? 0 : null)),
+    })
+  }
+
+  write(): void {
+    // We need to call this method from inside the WASM runtime and thus need synchronous I/O
+    fs.mkdirSync(COVERAGE_FOLDER, { recursive: true })
+    fs.writeFileSync(COVERAGE_FILE, JSON.stringify(this.data))
+  }
+}
+
+export function recordCoverage(file: string, from_line: number, to_line: number): void {
+  const report = new Report(JSON.parse(fs.readFileSync(COVERAGE_FILE, 'utf8')) as ReportData)
+  const file_report = report.data.source_files.find((f) => f.name === file)
+  if (!file_report) {
+    throw new Error('Could not find coverage entry for ' + file)
+  }
+  for (let line = from_line; line <= to_line; line++) {
+    file_report.coverage[line] = (file_report.coverage[line] || 0) + 1
+  }
+  report.write()
+}
